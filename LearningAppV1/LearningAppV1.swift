9b47f3c033f62640549db44130563c5d8d2d42cb
ktgcreative <ktgcreative@gmail.com>
2022-08-15 18:23:48 +1200

    - organise files into folders - add content model - set content model as @EnviromentObject

--- LearningAppV1/LearningAppV1.swift
+++ LearningAppV1/LearningAppV1.swift
@@ -1,5 +1,5 @@
 //
-//  LearningAppV1App.swift
+//  LearningAppV1.swift
 //  LearningAppV1
 //
 //  Created by Kyle on 15/08/2022.
@@ -8,10 +8,11 @@
 import SwiftUI
 
 @main
-struct LearningAppV1App: App {
+struct LearningAppV1: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
+                .environmentObject(ContentModel())
         }
     }
 }
---
+++ LearningAppV1/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  LearningAppV1
+//
+//  Created by Kyle on 15/08/2022.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
