7b5abc973bbcfffc2a3675c1933a3502ccdf76c4
ktgcreative <ktgcreative@gmail.com>
2022-08-15 18:12:15 +1200

    Initial Commit

---
+++ LearningAppV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningAppV1
+//
+//  Created by Kyle on 15/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LearningAppV1/LearningAppV1App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppV1App.swift
+//  LearningAppV1
+//
+//  Created by Kyle on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
